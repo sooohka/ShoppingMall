e6783d8456813a975f9e1ef94fd4875fcc76e348
강수호(sooohka) <shkorea1004@gmail.com>
2022-03-16 11:42:32 +0900

    chore: eslint rule추가, 절대경로 파일 추가

--- craco.config.js
+++ craco.config.js
@@ -2,25 +2,31 @@
 const CracoAlias = require("craco-alias");
 
 module.exports = {
-    jest: {
-        configure: {
-            moduleNameMapper: {
-                // Jest module mapper which will detect our absolute imports.
-                "@Assets/(.*)": '<rootDir>/src/assets/$1',
-                "@Components/(.*)": '<rootDir>/src/components/$1',
-                "@Config/(.*)": '<rootDir>/src/config/$1',
-                "@Domains/(.*)": '<rootDir>/src/domains/$1',
-                "@Hooks/(.*)": '<rootDir>/src/hooks/$1',
-                "@Providers/(.*)": '<rootDir>/src/providers/$1',
-                "@Routes/(.*)": '<rootDir>/src/routes/$1',
-                "@Types/(.*)": '<rootDir>/src/types/$1',
-                "@Utils/(.*)": '<rootDir>/src/utils/$1'
-            }
-        }
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        // Jest module mapper which will detect our absolute imports.
+        "@Assets/(.*)": "<rootDir>/src/assets/$1",
+        "@Components/(.*)": "<rootDir>/src/components/$1",
+        "@Config/(.*)": "<rootDir>/src/config/$1",
+        "@Domains/(.*)": "<rootDir>/src/domains/$1",
+        "@Hooks/(.*)": "<rootDir>/src/hooks/$1",
+        "@Providers/(.*)": "<rootDir>/src/providers/$1",
+        "@Routes/(.*)": "<rootDir>/src/routes/$1",
+        "@Types/(.*)": "<rootDir>/src/types/$1",
+        "@Utils/(.*)": "<rootDir>/src/utils/$1",
+        "@Misc/(.*)": "<rootDir>/src/misc/$1",
+        "@Contexts/(.*)": "<rootDir>/src/contexts/$1",
+      },
     },
-    plugins: [{
-        plugin: CracoAlias, options: {
-            source: "tsconfig", tsConfigPath: "tsconfig.path.json",
-        },
-    },],
+  },
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "tsconfig",
+        tsConfigPath: "tsconfig.path.json",
+      },
+    },
+  ],
 };
